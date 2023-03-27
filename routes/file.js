17c3f3c0210b9a03e15051b1ff37561a38dc216a
Sushant <smalvadakar1@gmail.com>
2023-03-27 10:40:55 +0530

    file

--- routes/file.js
+++ routes/file.js
@@ -4,10 +4,7 @@ const middleware = require("../middleware");
 const Post = require("../models/post");
 
 router.get("/", (req, res) => {
-    res.render("file/index", {
-        posts: allposts.reverse(),
-        currentUser: req.user,
-    });
+    res.render("file/index");
   });
 
 module.exports = router;
